8ba889c03e981f7f700af55843e764c6770c0b45
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-04-18 22:26:28 +0700

    error validation

--- index.js
+++ index.js
@@ -98,12 +98,8 @@ const generateId = () => {
     return maxId + 1
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (body.name === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
   
     // if (!body.name || !body.number) {
     //   return response.status(400).json({ 
@@ -120,12 +116,15 @@ app.post('/api/persons', (request, response) => {
     const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
     })
   
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedNote => {
+        response.json(savedAndFormattedNote)
+    }) 
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -148,7 +147,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
   
     next(error)
 }
