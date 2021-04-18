d4077dab37df049ae155895307021d3ffad8226e
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-04-18 23:11:02 +0700

    Adding ESLint

--- models/person.js
+++ models/person.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 var uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -14,17 +15,17 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, minlength: 3},
-  number: { type: String, required: true, minlength: 8},
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
 })
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports= mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
+// eslint-disable-next-line no-undef
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
+  // eslint-disable-next-line no-undef
   process.exit(1)
 }
 
+// eslint-disable-next-line no-undef
 const password = process.argv[2]
+// eslint-disable-next-line no-undef
 const name = process.argv[3]
+// eslint-disable-next-line no-undef
 const number = process.argv[4]
 
 const url =
@@ -27,19 +32,22 @@ const person = new Person({
 })
 
 
+// eslint-disable-next-line no-undef
 if (process.argv.length === 5) {
-    person.save().then(result => {
+  // eslint-disable-next-line no-unused-vars
+  person.save().then(result => {
     console.log(`added ${name} number ${number} to phonebook`)
     mongoose.connection.close()
-    })
+  })
+// eslint-disable-next-line no-undef
 } else if (process.argv.length === 3) {
-    console.log('phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-          console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else {
-
+  console.log('Invalid Input')
 }
